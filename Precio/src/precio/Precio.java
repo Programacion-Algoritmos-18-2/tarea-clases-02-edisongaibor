ae40497b81e77e5c43142b845846f360347c547f
Edison Gaibor Conde <edisongaiborconde@hotmail.com>
2018-10-12 10:22:42 -0500

    Agregando ejercicios

---
+++ Precio/src/precio/Precio.java
@@ -0,0 +1,19 @@
+package precio;
+
+/**
+ *
+ * @author ediso
+ */
+public class Precio {
+
+    double euros;
+
+    public double obtener() {
+       return euros;
+    }
+
+    public void agregar(double x) {
+
+    }
+
+}
